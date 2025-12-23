1b8902ce84c6e52f707fc60bf0cd49eaadf36c39
Hoang Quoc Phuong <qphwng2005@gmail.com>
2025-12-23 14:21:29 +0700

    Update footer.php
    Thêm link google map của trường.

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,12 @@
     </div>
     <footer class="bg-dark text-white text-center py-3 mt-4">
         <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> Bản quyền thuộc về Nhóm 3-Lớp 74DCTT28-Trường Đại học Công nghệ Giao thông Vận tải</p>
+            <p>&copy; <?php echo date('Y'); ?> Bản quyền thuộc về Nhóm 3 - Lớp 74DCTT28 -
+                <a href="https://maps.app.goo.gl/jxJ9PhbVPVnf1Daz9"
+                    target="_blank" style="text-decoration: none; color: white;">
+                    Trường Đại học Công nghệ Giao thông Vận tải
+                </a>
+            </p>
         </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
