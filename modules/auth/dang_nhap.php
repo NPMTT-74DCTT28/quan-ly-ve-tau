2d35afa7bc98a61fb51c65500c1734e0085b774d
Hoang Quoc Phuong <qphwng2005@gmail.com>
2025-12-24 01:00:38 +0700

    Update session management

--- modules/auth/dang_nhap.php
+++ modules/auth/dang_nhap.php
@@ -1,5 +1,11 @@
 <?php
 require_once __DIR__ . '/../../bootstrap.php';
+
+if (isset($_SESSION['user'])) {
+    header("Location: " . BASE_URL);
+    exit();
+}
+
 $conn = $db->getConnection();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
--- modules/auth/dang_xuat.php
+++ modules/auth/dang_xuat.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../../bootstrap.php';
 
 $_SESSION = array();
-
+session_unset();
 session_destroy();
 
 header("Location: " . BASE_URL . "modules/auth/dang_nhap.php");
