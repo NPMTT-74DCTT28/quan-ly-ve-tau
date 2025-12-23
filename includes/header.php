88c5a00db917e22fd4d35b5e1e40ab5c404ce59b
Hoang Quoc Phuong <qphwng2005@gmail.com>
2025-12-23 18:30:14 +0700

    Cập nhật layout chung, style css, thay đổi cấu hình, v.v

--- includes/header.php
+++ includes/header.php
@@ -5,27 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo SITE_NAME ?></title>
-    <link rel="stylesheet" href="../../quan-ly-ban-ve-tau/assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL ?>assets/css/style.css">
 </head>
 
 <body>
     <div>
         <nav class="nav">
             <div class="nav-items">
-                <a href="<?php echo BASE_URL ?>"><img src="../../quan-ly-ban-ve-tau/assets/images/logo.png" alt="logo" class="logo"></a>
+                <a href="<?php echo BASE_URL ?>"><img src="<?php echo BASE_URL ?>assets/images/logo.png" alt="logo" class="logo"></a>
                 <ul>
-                    <li><a href="../../quan-ly-ban-ve-tau/modules/nhan-vien/">Nhân viên</a></li>
-                    <li><a href="../../quan-ly-ban-ve-tau/modules/tau/">Tàu</a></li>
-                    <li><a href="../../quan-ly-ban-ve-tau/modules/loai-toa/">Loại toa</a></li>
-                    <li><a href="../../quan-ly-ban-ve-tau/modules/toa-tau/">Toa tàu</a></li>
-                    <li><a href="../../quan-ly-ban-ve-tau/modules/ghe/">Ghế</a></li>
-                    <li><a href="../../quan-ly-ban-ve-tau/modules/ga-tau/">Ga tàu</a></li>
-                    <li><a href="../../quan-ly-ban-ve-tau/modules/tuyen-duong/">Tuyến đường</a></li>
-                    <li><a href="../../quan-ly-ban-ve-tau/modules/lich-trinh/">Lịch trình</a></li>
-                    <li><a href="../../quan-ly-ban-ve-tau/modules/khach-hang/">Khách hàng</a></li>
-                    <li><a href="../../quan-ly-ban-ve-tau/modules/ve-tau/">Vé tàu</a></li>
-                    <li><a href="../../quan-ly-ban-ve-tau/modules/auth/dang_nhap.php">Đăng nhập</a></li>
+                    <li><a href="<?php echo BASE_URL ?>modules/nhan-vien/">Nhân viên</a></li>
+                    <li><a href="<?php echo BASE_URL ?>modules/tau/">Tàu</a></li>
+                    <li><a href="<?php echo BASE_URL ?>modules/toa-tau/">Toa tàu</a></li>
+                    <li><a href="<?php echo BASE_URL ?>modules/ghe/">Ghế</a></li>
+                    <li><a href="<?php echo BASE_URL ?>modules/ga-tau/">Ga tàu</a></li>
+                    <li><a href="<?php echo BASE_URL ?>modules/tuyen-duong/">Tuyến đường</a></li>
+                    <li><a href="<?php echo BASE_URL ?>modules/lich-trinh/">Lịch trình</a></li>
+                    <li><a href="<?php echo BASE_URL ?>modules/khach-hang/">Khách hàng</a></li>
+                    <li><a href="<?php echo BASE_URL ?>modules/ve-tau/">Vé tàu</a></li>
+                    <li><a href="<?php echo BASE_URL ?>modules/dang-nhap/">Đăng nhập</a></li>
                 </ul>
             </div>
         </nav>
     </div>
+    <div class="main-content">
