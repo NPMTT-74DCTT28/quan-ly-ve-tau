6e1f5a1a90c8d01c759f8119852b5aa0d9798ce8
Hoang Quoc Phuong <qphwng2005@gmail.com>
2026-01-14 13:53:14 +0700

    Cập nhật phân quyền
    - Yêu cầu đăng nhập để thấy được các chức năng của hệ thống.

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/includes/header.php';
-require_once __DIR__ . '/modules/thong-ke/ThongKe.php';
 ?>
-
+<div class="container">
+    <h1 style="text-align: center;">Chào mừng đến với website Hệ thống quản lý bán vé tàu</h1>
+</div>
 <?php require_once __DIR__ . '/includes/footer.php'; ?>
